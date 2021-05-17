0aa54ba3038ef048a9d27fe00169eeaa68c38b7c
jinho1011 <gino9940@gmail.com>
2021-05-17 16:07:07 +0900

    fix url

--- scoring/views.py
+++ scoring/views.py
@@ -90,7 +90,7 @@ class KakaotalkMessageView(View):
                 response.set_cookie(key='k'+str(step), value=-1)
                 return response
         elif step == 5 and result == 1:
-            URL = "https://www.figma.com/proto/ZV42t1vudbKH1nxSPCPy3p/%EC%82%B0%ED%98%91%ED%94%84-%EA%B3%B5%EC%9C%A0%EA%B3%B5%EA%B0%84_%EC%95%88%EB%93%9C%EB%9D%BC%EA%B3%A0%EC%A7%80?page-id=636%3A8&node-id=636%3A7605&viewport=431%2C1773%2C0.2996300160884857&scaling=min-zoom"
+            URL = "https://www.figma.com/proto/ZV42t1vudbKH1nxSPCPy3p/%EC%82%B0%ED%98%91%ED%94%84-%EA%B3%B5%EC%9C%A0%EA%B3%B5%EA%B0%84_%EC%95%88%EB%93%9C%EB%9D%BC%EA%B3%A0%EC%A7%80?page-id=636%3A8&node-id=636%3A7258&viewport=431%2C1773%2C0.2996300160884857&scaling=min-zoom"
             response = redirect(URL)
             try:
                 res = request.COOKIES.get('k'+str(step))
