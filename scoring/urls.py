9842dda814a826c2d8e674d489187ef55303a759
jinho1011 <gino9940@gmail.com>
2021-05-17 14:39:39 +0900

    Add step 1

--- scoring/urls.py
+++ scoring/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'scoring'
 
 urlpatterns = [
-    path('kakaotalk/1/r', views.k1r.as_view(), name='index'),
-    path('kakaotalk/1/w', views.k1w.as_view(), name='index'),
+    path('kakaotalk/<int:step>/<int:result>',
+         views.KakaotalkMessageView.as_view(), name='index'),
 ]
